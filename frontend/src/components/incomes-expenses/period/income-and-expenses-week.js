4a37bdd94f47ad2aac45ffd48a21d12f76a218b7
Nikita Zalevskii <nzalevskii@yandex.ru>
2024-08-27 10:22:42 +0300

    step3 v1.014 intervals3

--- frontend/src/components/incomes-expenses/period/income-and-expenses-week.js
+++ frontend/src/components/incomes-expenses/period/income-and-expenses-week.js
@@ -7,14 +7,22 @@ export class IncomeAndExpensesWeek {
 
         const date = new Date(), y = date.getFullYear(), m = date.getMonth();
         const dayAtWeek = date.getDay() - 1;
+        const dayLastAtWeek = 7 - date.getDay();
+        // const firstDayWeek =  new Date(y, m, date.getDate() - dayAtWeek + 1).toISOString().slice(0, 10);
         const firstDayWeek =  new Date(y, m, date.getDate() - dayAtWeek + 1).toISOString().slice(0, 10);
-        const lastDayWeek =  new Date(y, m, date.getDate() + 1).toISOString().slice(0, 10);
+        // const lastDayWeek =  new Date(y, m, date.getDate() + 1).toISOString().slice(0, 10);
+        const lastDayWeek =  new Date(y, m, date.getDate() + 1 + dayLastAtWeek).toISOString().slice(0, 10);
 
         document.getElementById('week').classList.add('flow-period-active');
         document.getElementById('today').classList.remove('flow-period-active');
         document.getElementById('month').classList.remove('flow-period-active');
         document.getElementById('year').classList.remove('flow-period-active');
         document.getElementById('all-period').classList.remove('flow-period-active');
+
+        // console.log(dayAtWeek);
+        // console.log(daylastAtWeek);
+        // console.log(firstDayWeek);
+        // console.log(lastDayWeek);
 
         IncomeAndExpensesPeriods.getOperations(firstDayWeek, lastDayWeek).then();
 
